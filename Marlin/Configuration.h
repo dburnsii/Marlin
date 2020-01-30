2e80561d1b9982028d9f08efa633cbd4782ac60b
Desone Burns II <dburnsii@live.com>
2020-01-30 08:06:31 -0700

    Added babystepping and speaker

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1712,7 +1712,7 @@
 // If you have a speaker that can produce tones, enable it here.
 // By default Marlin assumes you have a buzzer with a fixed frequency.
 //
-//#define SPEAKER
+#define SPEAKER
 
 //
 // The duration and frequency for the UI feedback sound.
